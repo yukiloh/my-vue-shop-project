5a04220b2b262bc7472c266df06f89a1c2bb3e7b
Ash <yukiloh@qq.com>
2020-03-27 18:47:03 +0800

    update

--- login-server/src/main/java/com/myvueshopproject/login/controller/LoginController.java
+++ login-server/src/main/java/com/myvueshopproject/login/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/my-vue-shop-project/")
+@RequestMapping("/my-vue-shop-project")
 public class LoginController {
 
     @PostMapping("/login")
