67ff8f28b5ddf55ba7d3456e62f84d7f76e7b42f
Ash <yukiloh@qq.com>
2020-03-29 00:11:21 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,30 @@
 <template>
 
-  <el-button type="info" @click="logout">退出</el-button>
+  <!-- 后台管理的主页面,分为上banner,左menu,右body,右下foot的布局-->
+  <!-- 可以直接从elementUI的 Container 中找到合适的布局  -->
+  <!-- 插入el标签后依然需要区element.js中导入相关组件-->
+
+  <el-container class="home-container">
+
+    <!-- 头部-->
+    <el-header>
+      Header
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+
+    <!-- 主体 -->
+    <el-container>
+
+      <!--侧边栏-->
+      <el-aside width="200px">Aside</el-aside>
+
+      <!--右侧主体-->
+      <el-container>
+        <el-main>Main</el-main>
+      </el-container>
+      
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -18,6 +42,26 @@
   }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+  /*可以直接用el标签来获取*/
+  .el-header {
+    background: #373744;
+  }
+
+  .el-aside {
+    background: #373744;
+  }
+
+  .el-main {
+    background: #eaedf1;
+  }
+
+  /*通过F12可以看到div#app下存在标签 section data-v-8dc7cce2="" class="el-container...*/
+  /*需要让该标签高度设为100%才能让home页面撑满元素;*/
+  /*没有选择用container的原因可能是考虑会冲突*/
+  .home-container {
+    height: 100%;
+  }
+
 
 </style>
