2c554fd40b425f60fc8c7f24e23badeef616e57b
Ash <yukiloh@qq.com>
2020-03-30 18:42:01 +0800

    update

--- login-server/src/main/java/com/myvueshopproject/login/controller/MainController.java
+++ login-server/src/main/java/com/myvueshopproject/login/controller/MainController.java
@@ -67,18 +67,20 @@ public class MainController {
             @RequestParam(name="pagesize", required=false) Integer pageSize
     ) {
 
-
-
-
-
         UsersResult.Users admin = new UsersResult.Users(777, "admin", "777", 1, "admin@admin.com", new Date(), true, "超级管理员");
         ArrayList<UsersResult.Users> list = new ArrayList<>();
         list.add(admin);
 
-        UsersResult.Users manager = null;
+        //手动实现分页...
         if (pageSize == null || pageSize != 1) {
-            manager = new UsersResult.Users(666, "manager", "666", 2, "mng@mng.com", new Date(), true, "管理员");
+            UsersResult.Users manager = new UsersResult.Users(666, "manager", "666", 2, "mng@mng.com", new Date(), true, "管理员");
             list.add(manager);
+
+            //如果既让每页显示1个又请求第二页,那么就返回只带第二页的data...
+            if (pagenum == 2) {
+                list.remove(admin);
+                return new UsersResult( new UsersResult.Data(list.size(), 2, list));
+            }
         }
 
         return new UsersResult( new UsersResult.Data(list.size(), 1, list));
