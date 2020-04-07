995691feb4428bccd8b6e7bea3ae7136adbd4c5c
Ash <yukiloh001@gmail.com>
2020-04-07 19:48:25 +0800

    update

--- src/components/goods/Goods.vue
+++ src/components/goods/Goods.vue
@@ -1,7 +1,25 @@
 <template>
   <div>
-    商品
+    <!-- 面包屑导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
 
+
+    <!-- 卡片区-->
+
+    <el-card class="box-card">
+
+      <!--分2行-->
+
+      <!--搜索input,添加商品btn-->
+
+      <!--商品列表form-->
+
+
+    </el-card>
   </div>
 </template>
 
